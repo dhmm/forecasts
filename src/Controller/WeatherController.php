d4f985302b68f6e786c3002120e4bd19c757a7cf
dhmm <dhmm@outlook.com>
2024-06-11 16:14:45 +0300

    parameters requirements added

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -9,8 +9,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController
 {
-    #[Route('/weather/highlander-says/{threshold}', name:'weather_higlander_says', methods:['GET|POST'])]
-    public function highlanderSays(int $threshold) : Response
+    #[Route('/weather/highlander-says/{threshold}', requirements: ['threshold' => '\d+'])]
+    public function highlanderSays(int $threshold = 50) : Response
     {
         //draw an integer from 0 to 100
         $draw = random_int(0,100);
@@ -18,6 +18,18 @@ class WeatherController extends AbstractController
         //if the value is < 50 (%) then say it's gonna rain
         //otherwise say it's gonna be sunny
         $forecast = $draw < $threshold ? "It's going to rain" : "It's going to be sunny";
+
+        //return response
+        return $this->render('weather/highlander_says.html.twig',  [
+                'forecast' => $forecast
+            ]
+        );
+    }
+
+    #[Route('/weather/highlander-says/{guess}')]
+    public function highlanderSaysGuess(string $guess) : Response
+    {
+        $forecast = "It's going to $guess";
 
         //return response
         return $this->render('weather/highlander_says.html.twig',  [
