3de8f19879a5c440762d5c80cc058b25607cc3f9
dhmm <dhmm@outlook.com>
2024-07-22 16:51:09 +0300

    validaiton rules only for the name field added

--- src/Entity/Location.php
+++ src/Entity/Location.php
@@ -16,7 +16,7 @@ class Location
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255)]    
     private ?string $name = null;
 
     #[ORM\Column(length: 2)]
