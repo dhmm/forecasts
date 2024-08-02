aebe61c2ac58117b2fb01f51fef51512521af0d6
dhmm <dhmm@outlook.com>
2024-08-02 16:29:10 +0300

    using serializer

--- src/Controller/WeatherApiController.php
+++ src/Controller/WeatherApiController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Encoder\YamlEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/v1/weather')]
 class WeatherApiController extends AbstractController
@@ -36,7 +38,7 @@ class WeatherApiController extends AbstractController
 
         return $response;
     }
-    #[Route('/jsont/{id}', name: 'app_weather_api')]
+    #[Route('/jsont/{id}')]
     public function jsonTwigAction(Location $location): Response
     {
         $content = $this->renderView('weather_api/json_twig.json.twig', [
@@ -47,7 +49,7 @@ class WeatherApiController extends AbstractController
 
         return $response;
     }
-    #[Route('/csvt/{id}', name: 'app_weather_api')]
+    #[Route('/csvt/{id}')]
     public function csvTwigAction(Location $location): Response
     {
         $content = $this->renderView('weather_api/csv_twig.csv.twig', [
@@ -57,5 +59,20 @@ class WeatherApiController extends AbstractController
         $response->headers->set('Content-Type' , 'text/csv');
 
         return $response;
+    }
+
+    #[Route('/serializer/{id}')]
+    public function serializerAction(
+        Location $location,
+        SerializerInterface $serializer,
+
+    ): Response
+    {
+        // $content = $serializer->serialize($location, 'json');
+        // $response  = new Response($content);
+        // $response->headers->set('Content-Type' , 'application/json');
+
+        // return $response;
+        return $this->json($location);
     }
 }
